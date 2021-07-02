3fd159c9e63d15185018a38f33fde1f95c8458f3
timjen3 <timjen3@gmail.com>
2021-07-01 21:19:51 -0400

    combat demo refactoring (#25)

---
+++ TTRPG.Engine.Demo/Demo/CombatRoles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TTRPG.Engine.Demo.Demo
+{
+	public static class CombatRoles
+	{
+		public static Role Player => new Role("Player", new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+		{
+			{ "MAX_HP", "20" },
+			{ "HP", "20" },
+			{ "Str", "18" },
+			{ "Dex", "18" },
+			{ "Ac", "2" },
+			{ "Con", "12" },
+			{ "Potions", "5" },
+			{ "Weapon.Hits", "1" },
+			{ "Weapon.Min", "1" },
+			{ "Weapon.Max", "6" }
+		});
+
+		public static Role Computer => new Role("Bandit", new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+		{
+			{ "MAX_HP", "50" },
+			{ "HP", "50" },
+			{ "Str", "18" },
+			{ "Dex", "14" },
+			{ "Ac", "2" },
+			{ "Con", "20" },
+			{ "Potions", "2" },
+			{ "Weapon.Hits", "1" },
+			{ "Weapon.Min", "1" },
+			{ "Weapon.Max", "8" }
+		});
+	}
+}
