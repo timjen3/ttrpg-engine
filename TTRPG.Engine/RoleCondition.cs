1ff7a60b0b92bce3d823102e45395ce01c709685
timjen3 <timjen3@gmail.com>
2022-09-21 23:11:56 -0400

    add role conditions to sequences (#53)
    add role conditions to sequences

---
+++ TTRPG.Engine/RoleCondition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TTRPG.Engine
+{
+	public class RoleCondition
+	{
+		public string RoleName { get; set; }
+
+		public List<string> RequiredCategories { get; set; } = new List<string>();
+	}
+}
