6b334bfb6a91c07d4545925cc1123b4f05a7ff55
Timothy <timjen3@gmail.com>
2022-11-04 21:22:39 -0400

    cleanup demo

---
+++ TTRPG.Engine.Demo2/Controls/GoodsDataGridItem.cs
@@ -0,0 +1,8 @@
+namespace TTRPG.Engine.Demo2.Controls
+{
+	internal class GoodsDataGridItem
+	{
+		public string Name { get; set; }
+		public string Amount { get; set; }
+	}
+}
--- TTRPG.Engine.Demo2/Controls/StatDataGridItem.cs
+++
@@ -1,8 +0,0 @@
-namespace TTRPG.Engine.Demo2.Controls
-{
-	internal class StatDataGridItem
-	{
-		public string Attribute { get; set; }
-		public string Value { get; set; }
-	}
-}
